07288a647bd36ca476a2edcc04cd1eb4199b8387
Mrbysco <Heeybysco@live.nl>
2016-11-05 18:16:01 +0100

    Temporary way before implementing TileEntities

--- src/main/java/com/svennieke/statues/blocks/BlockSnowGolem_Statue.java
+++ src/main/java/com/svennieke/statues/blocks/BlockSnowGolem_Statue.java
@@ -11,7 +11,6 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
@@ -25,7 +24,10 @@ import net.minecraft.world.World;
 public class BlockSnowGolem_Statue extends BaseCutout{
 	
 	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.0625 * 3, 0, 0.0625 * 3, 0.0625 * 13, 0.0625 * 12, 0.0625 * 13);
-
+	
+	private final String TAG_COOLDOWN = "cooldown";
+	public static double cooldown;
+	
 	public BlockSnowGolem_Statue() {
 		super(Material.TNT);
 		setUnlocalizedName(Reference.StatuesBlocks.SNOWGOLEMSTATUE.getUnlocalisedName());
@@ -38,12 +40,22 @@ public class BlockSnowGolem_Statue extends BaseCutout{
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
 			EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
-		playerIn.playSound(SoundEvents.ENTITY_SNOWMAN_AMBIENT, 1F, 1F);
+		cooldown = Math.random();
+		if (cooldown < 0.15) cooldown = StatueBehavior(this, playerIn);
+			
+			
+		//playerIn.playSound(SoundEvents.ENTITY_SNOWMAN_AMBIENT, 1F, 1F);
 		
 		//playerIn.dropItem(Items.SNOWBALL, 1);
 		//playerIn.dropItem(Blocks.PUMPKIN, 1);
 		return super.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY, hitZ);
 	}
+	
+	public int StatueBehavior(BlockSnowGolem_Statue statue, EntityPlayer playerIn) {
+		playerIn.playSound(SoundEvents.ENTITY_SNOWMAN_AMBIENT, 1F, 1F);
+		playerIn.dropItem(new ItemStack(Items.SNOWBALL, 1), true);
+		return 0;
+}
 	
 	@Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
