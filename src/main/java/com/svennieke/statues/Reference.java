89a4a8c8682fde14a29e0fd51aa28d787dae7089
Mrbysco <Heeybysco@live.nl>
2017-04-08 22:50:37 +0200

    3 new mobs enter

--- src/main/java/com/svennieke/statues/Reference.java
+++ src/main/java/com/svennieke/statues/Reference.java
@@ -19,6 +19,9 @@ public class Reference {
 		CREEPERSTATUE("creeperstatue", "BlockCreeperStatue"),
 		SNOWGOLEMSTATUE("snowgolemstatue", "BlockSnowGolemStatue"),
 		PIGSTATUE("pigstatue", "BlockpigStatue"),
+		RABBITSTATUE("rabbitstatue", "BlockrabbitStatue"),
+		SHEEPSTATUE("sheepstatue", "BlocksheepStatue"),
+		SHEEPSHAVENSTATUE("sheepshavenstatue", "BlocksheepshavenStatue"),
 		
 		SLIMESTATUET2("slimestatuet2", "BlockSlimeStatuet2"),
 		BLAZESTATUET2("blazestatuet2", "BlockBlazeStatuet2"),
@@ -29,6 +32,9 @@ public class Reference {
 		CREEPERSTATUET2("creeperstatuet2", "BlockCreeperStatuet2"),
 		SNOWGOLEMSTATUET2("snowgolemstatuet2", "BlockSnowGolemStatuet2"),
 		PIGSTATUET2("pigstatuet2", "BlockpigStatuet2"),
+		RABBITSTATUET2("rabbitstatuet2", "BlockrabbitStatuet2"),
+		SHEEPSTATUET2("sheepstatuet2", "BlocksheepStatuet2"),
+		SHEEPSHAVENSTATUET2("sheepshavenstatuet2", "BlocksheepshavenStatuet2"),
 		
 		SLIMESTATUET3("slimestatuet3", "BlockSlimeStatuet3"),
 		BLAZESTATUET3("blazestatuet3", "BlockBlazeStatuet3"),
@@ -39,6 +45,9 @@ public class Reference {
 		CREEPERSTATUET3("creeperstatuet3", "BlockCreeperStatuet3"),
 		SNOWGOLEMSTATUET3("snowgolemstatuet3", "BlockSnowGolemStatuet3"),
 		PIGSTATUET3("pigstatuet3", "BlockpigStatuet3"),
+		RABBITSTATUET3("rabbitstatuet3", "BlockrabbitStatuet3"),
+		SHEEPSTATUET3("sheepstatuet3", "BlocksheepStatuet3"),
+		SHEEPSHAVENSTATUET3("sheepshavenstatuet3", "BlocksheepshavenStatuet3"),
 		
 		SLIMESTATUET4("slimestatuet4", "BlockSlimeStatuet4"),
 		BLAZESTATUET4("blazestatuet4", "BlockBlazeStatuet4"),
@@ -48,7 +57,10 @@ public class Reference {
 		MOOSHROOMSTATUET4("mooshroomstatuet4", "BlockMooshroomStatuet4"),
 		CREEPERSTATUET4("creeperstatuet4", "BlockCreeperStatuet4"),
 		SNOWGOLEMSTATUET4("snowgolemstatuet4", "BlockSnowGolemStatuet4"),
-		PIGSTATUET4("pigstatuet4", "BlockpigStatuet4");
+		PIGSTATUET4("pigstatuet4", "BlockpigStatuet4"),
+		RABBITSTATUET4("rabbitstatuet4", "BlockrabbitStatuet4"),
+		SHEEPSTATUET4("sheepstatuet4", "BlocksheepStatuet4"),
+		SHEEPSHAVENSTATUET4("sheepshavenstatuet4", "BlocksheepshavenStatuet4");
 		
 		private String unlocalisedName;
 		private String registryName;
--- src/main/java/com/svennieke/statues/init/StatuesBlocks.java
+++ src/main/java/com/svennieke/statues/init/StatuesBlocks.java
@@ -7,6 +7,9 @@ import com.svennieke.statues.blocks.tiers.decorative.BlockCreeper_Statue_T1;
 import com.svennieke.statues.blocks.tiers.decorative.BlockKingCluck_Statue_T1;
 import com.svennieke.statues.blocks.tiers.decorative.BlockMooshroom_Statue_T1;
 import com.svennieke.statues.blocks.tiers.decorative.BlockPig_Statue_T1;
+import com.svennieke.statues.blocks.tiers.decorative.BlockRabbit_Statue_T1;
+import com.svennieke.statues.blocks.tiers.decorative.BlockSheepShaven_Statue_T1;
+import com.svennieke.statues.blocks.tiers.decorative.BlockSheep_Statue_T1;
 import com.svennieke.statues.blocks.tiers.decorative.BlockSlime_Statue_T1;
 import com.svennieke.statues.blocks.tiers.decorative.BlockSnowGolem_Statue_T1;
 import com.svennieke.statues.blocks.tiers.functional.BlockBlaze_Statue_T3;
@@ -16,6 +19,9 @@ import com.svennieke.statues.blocks.tiers.functional.BlockCreeper_Statue_T3;
 import com.svennieke.statues.blocks.tiers.functional.BlockKingCluck_Statue_T3;
 import com.svennieke.statues.blocks.tiers.functional.BlockMooshroom_Statue_T3;
 import com.svennieke.statues.blocks.tiers.functional.BlockPig_Statue_T3;
+import com.svennieke.statues.blocks.tiers.functional.BlockRabbit_Statue_T3;
+import com.svennieke.statues.blocks.tiers.functional.BlockSheepShaven_Statue_T3;
+import com.svennieke.statues.blocks.tiers.functional.BlockSheep_Statue_T3;
 import com.svennieke.statues.blocks.tiers.functional.BlockSlime_Statue_T3;
 import com.svennieke.statues.blocks.tiers.functional.BlockSnowGolem_Statue_T3;
 import com.svennieke.statues.blocks.tiers.muted.BlockBlaze_Statue_T4;
@@ -25,6 +31,9 @@ import com.svennieke.statues.blocks.tiers.muted.BlockCreeper_Statue_T4;
 import com.svennieke.statues.blocks.tiers.muted.BlockKingCluck_Statue_T4;
 import com.svennieke.statues.blocks.tiers.muted.BlockMooshroom_Statue_T4;
 import com.svennieke.statues.blocks.tiers.muted.BlockPig_Statue_T4;
+import com.svennieke.statues.blocks.tiers.muted.BlockRabbit_Statue_T4;
+import com.svennieke.statues.blocks.tiers.muted.BlockSheepShaven_Statue_T4;
+import com.svennieke.statues.blocks.tiers.muted.BlockSheep_Statue_T4;
 import com.svennieke.statues.blocks.tiers.muted.BlockSlime_Statue_T4;
 import com.svennieke.statues.blocks.tiers.muted.BlockSnowGolem_Statue_T4;
 import com.svennieke.statues.blocks.tiers.sounds.BlockBlaze_Statue_T2;
@@ -34,6 +43,9 @@ import com.svennieke.statues.blocks.tiers.sounds.BlockCreeper_Statue_T2;
 import com.svennieke.statues.blocks.tiers.sounds.BlockKingCluck_Statue_T2;
 import com.svennieke.statues.blocks.tiers.sounds.BlockMooshroom_Statue_T2;
 import com.svennieke.statues.blocks.tiers.sounds.BlockPig_Statue_T2;
+import com.svennieke.statues.blocks.tiers.sounds.BlockRabbit_Statue_T2;
+import com.svennieke.statues.blocks.tiers.sounds.BlockSheepShaven_Statue_T2;
+import com.svennieke.statues.blocks.tiers.sounds.BlockSheep_Statue_T2;
 import com.svennieke.statues.blocks.tiers.sounds.BlockSlime_Statue_T2;
 import com.svennieke.statues.blocks.tiers.sounds.BlockSnowGolem_Statue_T2;
 
@@ -47,20 +59,20 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class StatuesBlocks {
 	
 	public static Block blaze_statue, chicken_statue,cow_statue,creeper_statue, 
-						kingcluck_statue, mooshroom_statue, pig_statue,
-						slime_statue, snowgolem_statue;
+						kingcluck_statue, mooshroom_statue, pig_statue, rabbit_statue,
+						sheep_statue, sheepshaven_statue, slime_statue, snowgolem_statue;
 	
 	public static Block blaze_statuet2, chicken_statuet2, cow_statuet2,creeper_statuet2, 
-						kingcluck_statuet2, mooshroom_statuet2, pig_statuet2, slime_statuet2, 
-						snowgolem_statuet2;
+						kingcluck_statuet2, mooshroom_statuet2, pig_statuet2, rabbit_statuet2, 
+						sheep_statuet2, sheepshaven_statuet2, slime_statuet2, snowgolem_statuet2;
 	
 	public static Block blaze_statuet3, chicken_statuet3,cow_statuet3,creeper_statuet3, 
-						kingcluck_statuet3, mooshroom_statuet3, pig_statuet3, slime_statuet3, 
-							snowgolem_statuet3;
+						kingcluck_statuet3, mooshroom_statuet3, pig_statuet3, rabbit_statuet3, 
+						sheep_statuet3, sheepshaven_statuet3, slime_statuet3, snowgolem_statuet3;
 	
 	public static Block blaze_statuet4, chicken_statuet4,cow_statuet4,creeper_statuet4, 
-						kingcluck_statuet4, mooshroom_statuet4, pig_statuet4, slime_statuet4, 
-						snowgolem_statuet4;
+						kingcluck_statuet4, mooshroom_statuet4, pig_statuet4, rabbit_statuet4, 
+						sheep_statuet4, sheepshaven_statuet4, slime_statuet4, snowgolem_statuet4;
 	
 	public static void init()
 	{
@@ -71,6 +83,9 @@ public class StatuesBlocks {
 		kingcluck_statue = new BlockKingCluck_Statue_T1();
 		mooshroom_statue = new BlockMooshroom_Statue_T1();
 		pig_statue = new BlockPig_Statue_T1();
+		rabbit_statue = new BlockRabbit_Statue_T1();
+		sheepshaven_statue = new BlockSheepShaven_Statue_T1();
+		sheep_statue = new BlockSheep_Statue_T1();
 		slime_statue = new BlockSlime_Statue_T1();
 		snowgolem_statue = new BlockSnowGolem_Statue_T1();
 		
@@ -81,6 +96,9 @@ public class StatuesBlocks {
 		kingcluck_statuet2 = new BlockKingCluck_Statue_T2();
 		mooshroom_statuet2 = new BlockMooshroom_Statue_T2();
 		pig_statuet2 = new BlockPig_Statue_T2();
+		rabbit_statuet2 = new BlockRabbit_Statue_T2();
+		sheepshaven_statuet2 = new BlockSheepShaven_Statue_T2();
+		sheep_statuet2 = new BlockSheep_Statue_T2();
 		slime_statuet2 = new BlockSlime_Statue_T2();
 		snowgolem_statuet2 = new BlockSnowGolem_Statue_T2();
 		
@@ -91,6 +109,9 @@ public class StatuesBlocks {
 		kingcluck_statuet3 = new BlockKingCluck_Statue_T3();
 		mooshroom_statuet3 = new BlockMooshroom_Statue_T3();
 		pig_statuet3 = new BlockPig_Statue_T3();
+		rabbit_statuet3 = new BlockRabbit_Statue_T3();
+		sheepshaven_statuet3 = new BlockSheepShaven_Statue_T3();
+		sheep_statuet3 = new BlockSheep_Statue_T3();
 		slime_statuet3 = new BlockSlime_Statue_T3();
 		snowgolem_statuet3 = new BlockSnowGolem_Statue_T3();
 		
@@ -101,6 +122,9 @@ public class StatuesBlocks {
 		kingcluck_statuet4 = new BlockKingCluck_Statue_T4();
 		mooshroom_statuet4 = new BlockMooshroom_Statue_T4();
 		pig_statuet4 = new BlockPig_Statue_T4();
+		rabbit_statuet4 = new BlockRabbit_Statue_T4();
+		sheepshaven_statuet4 = new BlockSheepShaven_Statue_T4();
+		sheep_statuet4 = new BlockSheep_Statue_T4();
 		slime_statuet4 = new BlockSlime_Statue_T4();
 		snowgolem_statuet4 = new BlockSnowGolem_Statue_T4();
 	}
@@ -114,6 +138,9 @@ public class StatuesBlocks {
 		registerBlock(kingcluck_statue);
 		registerBlock(mooshroom_statue);
 		registerBlock(pig_statue);
+		registerBlock(rabbit_statue);
+		registerBlock(sheepshaven_statue);
+		registerBlock(sheep_statue);
 		registerBlock(slime_statue);
 		registerBlock(snowgolem_statue);
 		
@@ -124,6 +151,9 @@ public class StatuesBlocks {
 		registerBlock(kingcluck_statuet2);
 		registerBlock(mooshroom_statuet2);
 		registerBlock(pig_statuet2);
+		registerBlock(rabbit_statuet2);
+		registerBlock(sheepshaven_statuet2);
+		registerBlock(sheep_statuet2);
 		registerBlock(slime_statuet2);
 		registerBlock(snowgolem_statuet2);
 		
@@ -134,6 +164,9 @@ public class StatuesBlocks {
 		registerBlock(kingcluck_statuet3);
 		registerBlock(mooshroom_statuet3);
 		registerBlock(pig_statuet3);
+		registerBlock(rabbit_statuet3);
+		registerBlock(sheepshaven_statuet3);
+		registerBlock(sheep_statuet3);
 		registerBlock(slime_statuet3);
 		registerBlock(snowgolem_statuet3);
 		
@@ -144,6 +177,9 @@ public class StatuesBlocks {
 		registerBlock(kingcluck_statuet4);
 		registerBlock(mooshroom_statuet4);
 		registerBlock(pig_statuet4);
+		registerBlock(rabbit_statuet4);
+		registerBlock(sheepshaven_statuet4);
+		registerBlock(sheep_statuet4);
 		registerBlock(slime_statuet4);
 		registerBlock(snowgolem_statuet4);
 	}
@@ -169,6 +205,9 @@ public class StatuesBlocks {
 		registerRender(kingcluck_statue);
 		registerRender(mooshroom_statue);
 		registerRender(pig_statue);
+		registerRender(rabbit_statue);
+		registerRender(sheepshaven_statue);
+		registerRender(sheep_statue);
 		registerRender(slime_statue);
 		registerRender(snowgolem_statue);
 		
@@ -179,6 +218,9 @@ public class StatuesBlocks {
 		registerRender(kingcluck_statuet2);
 		registerRender(mooshroom_statuet2);
 		registerRender(pig_statuet2);
+		registerRender(rabbit_statuet2);
+		registerRender(sheepshaven_statuet2);
+		registerRender(sheep_statuet2);
 		registerRender(slime_statuet2);
 		registerRender(snowgolem_statuet2);
 		
@@ -189,6 +231,9 @@ public class StatuesBlocks {
 		registerRender(kingcluck_statuet3);
 		registerRender(mooshroom_statuet3);
 		registerRender(pig_statuet3);
+		registerRender(rabbit_statuet3);
+		registerRender(sheepshaven_statuet3);
+		registerRender(sheep_statuet3);
 		registerRender(slime_statuet3);
 		registerRender(snowgolem_statuet3);
 		
@@ -199,6 +244,9 @@ public class StatuesBlocks {
 		registerRender(kingcluck_statuet4);
 		registerRender(mooshroom_statuet4);
 		registerRender(pig_statuet4);
+		registerRender(rabbit_statuet4);
+		registerRender(sheepshaven_statuet4);
+		registerRender(sheep_statuet4);
 		registerRender(slime_statuet4);
 		registerRender(snowgolem_statuet4);
 	}
