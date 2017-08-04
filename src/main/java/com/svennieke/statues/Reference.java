335f504ffa4a131bdcec0abaf97a38e49b119ae5
Mrbysco <Heeybysco@live.nl>
2017-08-04 13:58:51 +0200

    -

--- src/main/java/com/svennieke/statues/Reference.java
+++ src/main/java/com/svennieke/statues/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_NAME = "Statues";
 	public static final String MOD_PREFIX = "statues:";
 	public static final String VERSION = "0.7.2";
-	public static final String ACCEPTED_VERSIONS = "[1.12]";
+	public static final String ACCEPTED_VERSIONS = "[1.12], [1.12.1]";
 	public static final String DEPENDENCIES = "after:Baubles;";
 			
 	public static final String CLIENT_PROXY_CLASS = "com.svennieke.statues.proxy.ClientProxy";
