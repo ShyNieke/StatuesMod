6368740eb44e6b3a7a018750ff9eff96b52699d6
Mrbysco <Mrbysco@users.noreply.github.com>
2020-08-26 22:38:18 +0200

    Fixed curios support

--- src/main/java/com/shynieke/statues/compat/curios/StatueCurioCapability.java
+++ src/main/java/com/shynieke/statues/compat/curios/StatueCurioCapability.java
@@ -4,8 +4,11 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3f;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
@@ -29,12 +32,25 @@ public class StatueCurioCapability implements ICurio {
     @Override
     public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         matrixStack.push();
-        Minecraft.getInstance().getRenderManager().getSkinMap().get("default").getEntityModel().getModelHead().translateRotate(matrixStack);
-        matrixStack.translate(0.0D, -0.25D, 0.0D);
-        matrixStack.rotate(Vector3f.YP.rotationDegrees(180.0F));
-        matrixStack.scale(0.65F, -0.65F, -0.65F);
 
-        Minecraft.getInstance().getFirstPersonRenderer().renderItemSide(livingEntity, this.displayedItem, ItemCameraTransforms.TransformType.HEAD, false, matrixStack, renderTypeBuffer, light);
+        translateToHeadLevel(livingEntity, matrixStack);
+        translateToFace(matrixStack);
+        matrixStack.translate(0.0F, 2.975F, 1.275F);
+        matrixStack.scale(0.625F, 0.625F, 0.625F);
+
+        Minecraft.getInstance().getItemRenderer().renderItem(this.displayedItem, ItemCameraTransforms.TransformType.HEAD, light, OverlayTexture.NO_OVERLAY, matrixStack, renderTypeBuffer);
         matrixStack.pop();
+    }
+
+    public static void translateToHeadLevel(LivingEntity livingEntity, MatrixStack matrixStack) {
+        matrixStack.translate(0, -livingEntity.getEyeHeight(), 0);
+        if (livingEntity.isSneaking())
+            matrixStack.translate(0.25F * MathHelper.sin(livingEntity.rotationPitch * (float) Math.PI / 180), 0.25F * MathHelper.cos(livingEntity.rotationPitch * (float) Math.PI / 180), 0F);
+    }
+
+    public static void translateToFace(MatrixStack matrixStack) {
+        matrixStack.rotate(Vector3f.YP.rotation(90F));
+        matrixStack.rotate(Vector3f.XP.rotation(180F));
+        matrixStack.translate(0f, -4.35f, -1.27f);
     }
 }
