85c86a3d5a58e5ed01015c2a3ea32550c26271f4
Mrbysco <Mrbysco@users.noreply.github.com>
2022-11-10 18:16:28 +0100

    Fix missing import

--- src/main/java/com/shynieke/statues/menu/ShulkerStatueMenu.java
+++ src/main/java/com/shynieke/statues/menu/ShulkerStatueMenu.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.items.SlotItemHandler;
 
 import javax.annotation.Nonnull;
