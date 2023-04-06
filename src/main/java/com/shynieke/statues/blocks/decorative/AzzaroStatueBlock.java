cac0c862910151dd875be9b10c675f62d3fe535e
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-06 16:22:33 +0200

    Add Azzaro statue
    Model courtesy of `_Hawkon`

---
+++ src/main/java/com/shynieke/statues/blocks/decorative/AzzaroStatueBlock.java
@@ -0,0 +1,49 @@
+package com.shynieke.statues.blocks.decorative;
+
+import com.shynieke.statues.blocks.AbstractStatueBase;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nullable;
+
+public class AzzaroStatueBlock extends AbstractStatueBase {
+
+	private static final VoxelShape SHAPE = Block.box(4, 0, 4, 12, 7, 12);
+
+	public AzzaroStatueBlock(Properties properties) {
+		super(properties.sound(SoundType.STONE));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	public SoundEvent getSound(BlockState state) {
+		return SoundEvents.WOOL_BREAK;
+	}
+
+	@Nullable
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+		return null;
+	}
+}
