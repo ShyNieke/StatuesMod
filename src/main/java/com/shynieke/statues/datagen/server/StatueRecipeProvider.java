7c7c1ad87a3243d47ef5f7a6d35306b97bc870a8
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-25 14:06:07 +0200

    Add loot for vindicators
    Stop giving items if it can't find loot for a statue and log the statue that has no loot

--- src/main/java/com/shynieke/statues/datagen/server/StatueRecipeProvider.java
+++ src/main/java/com/shynieke/statues/datagen/server/StatueRecipeProvider.java
@@ -183,6 +183,8 @@ public class StatueRecipeProvider extends RecipeProvider {
 				.result1(Items.BEEF).result2(Items.RED_MUSHROOM).result3(Items.RED_MUSHROOM_BLOCK).build(consumer);
 		LootRecipeBuilder.loot(Ingredient.of(StatueRegistry.BROWN_MOOSHROOM_STATUE.get()))
 				.result1(Items.BEEF).result2(Items.BROWN_MUSHROOM).result3(Items.BROWN_MUSHROOM_BLOCK).build(consumer);
+		LootRecipeBuilder.loot(Ingredient.of(StatueRegistry.VINDICATOR_STATUE.get()))
+				.result2(Items.EMERALD).build(consumer);
 
 		UpgradeRecipeBuilder.upgrade(Ingredient.of(StatueTags.UPGRADEABLE_STATUES), new ArrayList<>())
 				.requiresCore().upgradeType(UpgradeType.UPGRADE).build(consumer, new ResourceLocation(Reference.MOD_ID, "upgrade/statue_upgrade"));
