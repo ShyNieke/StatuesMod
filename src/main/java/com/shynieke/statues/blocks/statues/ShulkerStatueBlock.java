52a7973dd6947bbd0fe425c6e231d9161b7e8019
Mrbysco <Mrbysco@users.noreply.github.com>
2023-07-05 00:28:49 +0200

    Fix the Shulker statue not doing anything

--- src/main/java/com/shynieke/statues/blocks/statues/ShulkerStatueBlock.java
+++ src/main/java/com/shynieke/statues/blocks/statues/ShulkerStatueBlock.java
@@ -3,14 +3,18 @@ package com.shynieke.statues.blocks.statues;
 import com.shynieke.statues.blockentities.ShulkerStatueBlockEntity;
 import com.shynieke.statues.blockentities.StatueBlockEntity;
 import com.shynieke.statues.blocks.AbstractStatueBase;
+import com.shynieke.statues.registry.StatueBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -47,5 +51,19 @@ public class ShulkerStatueBlock extends AbstractStatueBase {
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
 		return SHAPE;
+	}
+
+	@Nullable
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+		if (state.getValue(INTERACTIVE).booleanValue()) {
+			return createStatueTicker(level, blockEntityType, StatueBlockEntities.SHULKER_STATUE.get());
+		} else {
+			return null;
+		}
+	}
+
+	@Nullable
+	protected static <T extends BlockEntity> BlockEntityTicker<T> createStatueTicker(Level level, BlockEntityType<T> blockEntityType, BlockEntityType<? extends StatueBlockEntity> blockEntityType1) {
+		return level.isClientSide ? null : createTickerHelper(blockEntityType, blockEntityType1, ShulkerStatueBlockEntity::serverTick);
 	}
 }
