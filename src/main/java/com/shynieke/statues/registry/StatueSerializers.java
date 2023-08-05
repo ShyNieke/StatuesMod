6b48e49f9c4cb1a93ef49030d1e772d53e68c17b
Mrbysco <Mrbysco@users.noreply.github.com>
2023-08-05 14:55:03 +0200

    Fix serializer
    `FriendlyByteBuf::readGameProfile` can crash when it's missing part of the GameProfile

--- src/main/java/com/shynieke/statues/registry/StatueSerializers.java
+++ src/main/java/com/shynieke/statues/registry/StatueSerializers.java
@@ -2,6 +2,8 @@ package com.shynieke.statues.registry;
 
 import com.mojang.authlib.GameProfile;
 import com.shynieke.statues.Reference;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,7 +15,21 @@ import java.util.Optional;
 public class StatueSerializers {
 	public static final DeferredRegister<EntityDataSerializer<?>> ENTITY_DATA_SERIALIZER = DeferredRegister.create(Keys.ENTITY_DATA_SERIALIZERS, Reference.MOD_ID);
 
-	public static final RegistryObject<EntityDataSerializer<Optional<GameProfile>>> OPTIONAL_GAME_PROFILE = ENTITY_DATA_SERIALIZER.register("optional_game_profile", () ->
-			EntityDataSerializer.optional(FriendlyByteBuf::writeGameProfile, FriendlyByteBuf::readGameProfile)
-	);
+	public static final RegistryObject<EntityDataSerializer<Optional<GameProfile>>> OPTIONAL_GAME_PROFILE = ENTITY_DATA_SERIALIZER.register("optional_game_profile", () -> new EntityDataSerializer<Optional<GameProfile>>() {
+		public void write(FriendlyByteBuf friendlyByteBuf, Optional<GameProfile> optionalGameProfile) {
+			friendlyByteBuf.writeBoolean(optionalGameProfile.isPresent());
+			if (optionalGameProfile.isPresent()) {
+				friendlyByteBuf.writeNbt(NbtUtils.writeGameProfile(new CompoundTag(), optionalGameProfile.get()));
+			}
+
+		}
+
+		public Optional<GameProfile> read(FriendlyByteBuf friendlyByteBuf) {
+			return !friendlyByteBuf.readBoolean() ? Optional.empty() : Optional.of(NbtUtils.readGameProfile(friendlyByteBuf.readNbt()));
+		}
+
+		public Optional<GameProfile> copy(Optional<GameProfile> optionalGameProfile) {
+			return optionalGameProfile;
+		}
+	});
 }
