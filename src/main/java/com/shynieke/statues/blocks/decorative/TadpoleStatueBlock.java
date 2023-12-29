5d0106b6483a869c3714e50e7ab349a7374accc0
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-29 17:05:09 +0100

    Add tadpole statue

---
+++ src/main/java/com/shynieke/statues/blocks/decorative/TadpoleStatueBlock.java
@@ -0,0 +1,41 @@
+package com.shynieke.statues.blocks.decorative;
+
+import com.shynieke.statues.blocks.AbstractStatueBase;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class TadpoleStatueBlock extends AbstractStatueBase {
+
+	private static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+
+	public TadpoleStatueBlock(Properties properties) {
+		super(properties.sound(SoundType.STONE).isRedstoneConductor(TadpoleStatueBlock::isntSolid));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	public SoundEvent getSound(BlockState state) {
+		return SoundEvents.TADPOLE_FLOP;
+	}
+
+	@Override
+	public EntityType<?> getEntity() {
+		return EntityType.TADPOLE;
+	}
+
+	private static boolean isntSolid(BlockState state, BlockGetter getter, BlockPos pos) {
+		return false;
+	}
+}
