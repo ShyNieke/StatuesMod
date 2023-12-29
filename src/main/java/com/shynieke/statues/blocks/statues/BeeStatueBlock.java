78943f37214df20518afa4b36dc84a982b2ffa98
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-29 18:12:42 +0100

    Remove unused imports

--- src/main/java/com/shynieke/statues/blocks/statues/BeeStatueBlock.java
+++ src/main/java/com/shynieke/statues/blocks/statues/BeeStatueBlock.java
@@ -1,9 +1,6 @@
 package com.shynieke.statues.blocks.statues;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
