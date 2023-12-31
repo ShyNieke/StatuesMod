22eb7efb9e0ebc944397680d8c2867a109e89b2d
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-31 01:20:47 +0100

    Add information to REI

--- src/main/java/com/shynieke/statues/compat/rei/REIPlugin.java
+++ src/main/java/com/shynieke/statues/compat/rei/REIPlugin.java
@@ -1,10 +1,14 @@
 package com.shynieke.statues.compat.rei;
 
 import com.shynieke.statues.Reference;
+import com.shynieke.statues.Statues;
+import com.shynieke.statues.blocks.AbstractStatueBase;
 import com.shynieke.statues.compat.rei.category.LootCategory;
 import com.shynieke.statues.compat.rei.category.UpgradeCategory;
 import com.shynieke.statues.compat.rei.display.LootDisplay;
 import com.shynieke.statues.compat.rei.display.UpgradeDisplay;
+import com.shynieke.statues.config.StatuesConfig;
+import com.shynieke.statues.items.StatueBlockItem;
 import com.shynieke.statues.recipe.LootRecipe;
 import com.shynieke.statues.recipe.StatuesRecipes;
 import com.shynieke.statues.recipe.UpgradeRecipe;
@@ -16,10 +20,15 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.forge.REIPluginClient;
+import me.shedaniel.rei.plugin.client.BuiltinClientPlugin;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
 
@@ -114,6 +123,60 @@ public class REIPlugin implements REIClientPlugin {
 					tier)
 			);
 		});
+
+
+		BuiltinClientPlugin clientPlugin = BuiltinClientPlugin.getInstance();
+		List<StatueBlockItem> statues = StatueRegistry.ITEMS.getEntries().stream()
+				.filter(registryObject -> registryObject.get() instanceof StatueBlockItem statueBlock &&
+						statueBlock.getBlock() instanceof AbstractStatueBase statueBase && !statueBase.isHiddenStatue())
+				.map(registryObject -> (StatueBlockItem) registryObject.get()).toList();
+		double chance = StatuesConfig.COMMON.statueDropChance.get();
+		for (StatueBlockItem statue : statues) {
+			if (statue.getEntity() == null) {
+				Statues.LOGGER.error("Tried adding info to statue but statue {} has no entity linked", BuiltInRegistries.ITEM.getKey(statue));
+			} else {
+				clientPlugin.registerInformation(EntryStacks.of(statue.getDefaultInstance()), Component.empty(),
+						list -> {
+							int chancePercentage = (int) (chance * 100);
+							if (statue.getEntity() == EntityType.WARDEN || statue.getEntity() == EntityType.ELDER_GUARDIAN) {
+								chancePercentage = 100;
+							} else if (statue.getEntity() == EntityType.RAVAGER) {
+								chancePercentage = 25;
+							}
+							list.add(Component.translatable("statues.gui.jei.statue.info",
+									Component.translatable(statue.getDescriptionId()).withStyle(ChatFormatting.YELLOW).withStyle(ChatFormatting.BOLD),
+									Component.literal(chancePercentage + "%").withStyle(ChatFormatting.DARK_GREEN).withStyle(ChatFormatting.BOLD),
+									Component.translatable(statue.getEntity().getDescriptionId()).withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD)));
+							return list;
+						}
+				);
+			}
+		}
+		clientPlugin.registerInformation(EntryStacks.of(StatueRegistry.STATUE_CORE.get().getDefaultInstance()), Component.empty(),
+				list -> {
+					list.add(Component.translatable("statues.gui.jei.statue_core.info",
+							Component.translatable(StatueRegistry.STATUE_CORE.get().getDescriptionId()).withStyle(ChatFormatting.YELLOW).withStyle(ChatFormatting.BOLD),
+							Component.translatable(StatueRegistry.STATUE_BAT.get().getDescriptionId()).withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD)
+					));
+					list.add(Component.translatable("statues.gui.jei.statue_core.info2",
+							Component.translatable(StatueRegistry.STATUE_BAT.get().getDescriptionId()).withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD)
+					));
+					return list;
+				}
+		);
+		clientPlugin.registerInformation(EntryStacks.of(StatueRegistry.STATUE_TABLE.get().asItem().getDefaultInstance()), Component.empty(),
+				list -> {
+					list.add(Component.translatable("statues.gui.jei.statue_table.info"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info2"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info3"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info4"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info5"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info6"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info7"));
+					list.add(Component.translatable("statues.gui.jei.statue_table.info8"));
+					return list;
+				}
+		);
 	}
 
 
