be81e2c20464f106a8ba592b39e730b254cb8c30
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-10 16:08:43 +0200

    Update Data component registry

--- src/main/java/com/shynieke/statues/registry/StatueDataComponents.java
+++ src/main/java/com/shynieke/statues/registry/StatueDataComponents.java
@@ -13,32 +13,32 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 public class StatueDataComponents {
-	public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, Reference.MOD_ID);
+	public static final DeferredRegister.DataComponents DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Reference.MOD_ID);
 
-	public static final Supplier<DataComponentType<PlayerCompassData>> PLAYER_COMPASS_DATA = DATA_COMPONENT_TYPES.register("player_compass_data", () ->
-			DataComponentType.<PlayerCompassData>builder()
+	public static final Supplier<DataComponentType<PlayerCompassData>> PLAYER_COMPASS_DATA = DATA_COMPONENT_TYPES.registerComponentType("player_compass_data", builder ->
+			builder
 					.persistent(PlayerCompassData.CODEC)
 					.networkSynchronized(PlayerCompassData.STREAM_CODEC)
-					.build());
+	);
 
-	public static final Supplier<DataComponentType<Boolean>> UPGRADED = DATA_COMPONENT_TYPES.register("upgraded", () ->
-			DataComponentType.<Boolean>builder()
+	public static final Supplier<DataComponentType<Boolean>> UPGRADED = DATA_COMPONENT_TYPES.registerComponentType("upgraded", builder ->
+			builder
 					.persistent(Codec.BOOL)
 					.networkSynchronized(ByteBufCodecs.BOOL)
-					.build());
+	);
 
-	public static final Supplier<DataComponentType<StatueStats>> STATS = DATA_COMPONENT_TYPES.register("stats", () ->
-			DataComponentType.<StatueStats>builder()
+	public static final Supplier<DataComponentType<StatueStats>> STATS = DATA_COMPONENT_TYPES.registerComponentType("stats", builder ->
+			builder
 					.persistent(StatueStats.CODEC)
 					.networkSynchronized(StatueStats.STREAM_CODEC)
 					.cacheEncoding()
-					.build());
+	);
 
-	public static final Supplier<DataComponentType<StatueUpgrades>> UPGRADES = DATA_COMPONENT_TYPES.register("upgrades", () ->
-			DataComponentType.<StatueUpgrades>builder()
+	public static final Supplier<DataComponentType<StatueUpgrades>> UPGRADES = DATA_COMPONENT_TYPES.registerComponentType("upgrades", builder ->
+			builder
 					.persistent(StatueUpgrades.CODEC)
 					.networkSynchronized(StatueUpgrades.STREAM_CODEC)
 					.cacheEncoding()
-					.build());
+	);
 
 }
