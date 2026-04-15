72b3ff9fd730e13bc0cd3a513acb35e241df4ecb
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-15 22:57:52 +0200

    -

--- src/main/java/com/shynieke/statues/datagen/StatuesDataGenerator.java
+++ src/main/java/com/shynieke/statues/datagen/StatuesDataGenerator.java
@@ -1,25 +1,28 @@
 package com.shynieke.statues.datagen;
 
-import com.shynieke.statues.Reference;
 import com.shynieke.statues.datagen.client.StatueLanguageProvider;
 import com.shynieke.statues.datagen.client.StatueModelProvider;
 import com.shynieke.statues.datagen.client.StatueSoundProvider;
 import com.shynieke.statues.datagen.server.StatueAdvancementProvider;
+import com.shynieke.statues.datagen.server.StatueBiomeModifiers;
 import com.shynieke.statues.datagen.server.StatueBiomeTagProvider;
 import com.shynieke.statues.datagen.server.StatueBlockTagProvider;
-import com.shynieke.statues.datagen.server.StatueDatapackProvider;
 import com.shynieke.statues.datagen.server.StatueGLMProvider;
 import com.shynieke.statues.datagen.server.StatueItemTagProvider;
 import com.shynieke.statues.datagen.server.StatueLootProvider;
 import com.shynieke.statues.datagen.server.StatueRecipeProvider;
+import com.shynieke.statues.registry.StatueJukeboxSongs;
+import com.shynieke.statues.registry.StatueTrims;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber
@@ -28,6 +31,7 @@ public class StatuesDataGenerator {
 	public static void gatherData(GatherDataEvent.Client event) {
 		DataGenerator generator = event.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
+		event.createDatapackRegistryObjects(BUILDER);
 		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
 		generator.addProvider(true, new StatueLanguageProvider(packOutput));
@@ -44,10 +48,14 @@ public class StatuesDataGenerator {
 //			generator.addProvider(true, new StatuePatchouliProvider(packOutput, lookupProvider));
 		generator.addProvider(true, new StatueAdvancementProvider(packOutput, lookupProvider));
 
-		generator.addProvider(true, new StatueDatapackProvider(
-				packOutput,
-				event.getLookupProvider(),
-				Set.of(Reference.MOD_ID)
-		));
 	}
+
+	public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+			.add(Registries.CONFIGURED_FEATURE, $ -> {
+			})
+			.add(Registries.PLACED_FEATURE, $ -> {
+			})
+			.add(Registries.TRIM_PATTERN, StatueTrims::bootstrap)
+			.add(Registries.JUKEBOX_SONG, StatueJukeboxSongs::bootstrap)
+			.add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, StatueBiomeModifiers::bootstrap);
 }
