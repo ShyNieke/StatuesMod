64b8bc24e327cca71f2d03bbc63dadde82a9fb3b
Mrbysco <Mrbysco@users.noreply.github.com>
2026-08-07 17:30:06 +0200

    Remove unused fields

--- src/main/java/com/shynieke/statues/Reference.java
+++ src/main/java/com/shynieke/statues/Reference.java
@@ -8,12 +8,8 @@ import java.util.UUID;
 
 public class Reference {
 	public static final String MOD_ID = "statues";
-	public static final String MOD_PREFIX = MOD_ID + ":";
 
-	public static final String KILL_COUNT = "statueMobKilled";
-	public static final String LEVEL = "statueLevel";
 	public static final String UPGRADED = "statueUpgraded";
-	public static final String UPGRADE_SLOTS = "upgradeSlots";
 	public static final GameProfile GAME_PROFILE = new GameProfile(UUID.nameUUIDFromBytes("fakeplayer.statue".getBytes()), Component.translatable("fakeplayer.statue").getString());
 
 	public static ResourceLocation modLoc(String path) {
